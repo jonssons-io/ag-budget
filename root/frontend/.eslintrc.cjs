159d55e62e319163727b89ef7df0198fa6658d4c
Gabrielle Jonsson <dev.gabriellejonsson@gmail.com>
2023-11-18 16:55:30 +0100

    Add Husky, ESLint, Prettier and Airbnb config

--- root/frontend/.eslintrc.cjs
+++ root/frontend/.eslintrc.cjs
@@ -2,17 +2,28 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-typescript",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "*.config.**"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  plugins: ["react-refresh", "prettier"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "react/jsx-one-expression-per-line": [2, { allow: "single-child" }],
   },
-}
+};
