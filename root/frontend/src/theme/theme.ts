4706ce14bbf0ad1782479c6c3f4f679179c1844c
Gabrielle Jonsson <dev.gabriellejonsson@gmail.com>
2023-11-19 15:23:16 +0100

    Add initial custom theme

---
+++ root/frontend/src/theme/theme.ts
@@ -0,0 +1,21 @@
+export const darkTokens = {
+  colorPrimary: "#cb0c9f",
+  colorInfo: "#17c1e8",
+  colorSuccess: "#82d616",
+  colorWarning: "#fbcf33",
+  colorError: "#ea0606",
+  colorBgBase: "#1b1b1b",
+  colorTextBase: "#a9b0bf",
+  colorLink: "#17c1e8",
+};
+
+export const lightTokens = {
+  colorPrimary: "#cb0c9f",
+  colorInfo: "#17c1e8",
+  colorSuccess: "#82d616",
+  colorWarning: "#fbcf33",
+  colorError: "#ea0606",
+  colorBgBase: "#f8f9fa",
+  colorTextBase: "#454d5e",
+  colorLink: "#17c1e8",
+};
