5b4f38ae2ca0826d72ab9a12a667ad63e31f0a98
Axel Jonsson <axel@jonssons.io>
2023-11-25 23:52:01 +0100

    added vercel postgres

--- root/backend/index.js
+++ root/backend/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const { getUserInfo } = require("./db/db_client");
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
+});
+
+app.get("/user", async (req, res) => {
+  let name = req.query.name ?? "-";
+  let userInfo = await getUserInfo(name);
+  console.log("Got user info:", userInfo);
+  res.send(userInfo);
 });
 
 app.listen(5000, () => {
