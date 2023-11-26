12b3599cd19e626f6c391ec99d41f5c89b5a5d43
Gabrielle Jonsson <dev.gabriellejonsson@gmail.com>
2023-11-26 03:39:09 +0100

    Move darkmode to jotai

--- root/frontend/src/App.tsx
+++ root/frontend/src/App.tsx
@@ -1,28 +1,12 @@
-import { ConfigProvider, theme } from "antd";
-import useDarkMode from "./hooks/useDarkMode";
-import { darkTokens, lightTokens } from "./theme/theme";
-import appOverrides from "./theme/componentsOverride";
-import Layout from "./layout/Layout";
-import withSuspense from "./util/hoc/withSuspense";
-
-const LayoutWithSuspense = withSuspense(Layout);
+import { Provider as StateProvider } from "jotai";
+import { RouterProvider } from "react-router-dom";
+import router from "./util/router/browserRouter";
 
 function App() {
-  const [darkMode, handleChangeMode] = useDarkMode();
-
   return (
-    <ConfigProvider
-      theme={{
-        algorithm: darkMode ? theme.darkAlgorithm : theme.defaultAlgorithm,
-        token: darkMode ? { ...darkTokens } : { ...lightTokens },
-        components: appOverrides(darkMode),
-      }}
-    >
-      <LayoutWithSuspense
-        darkMode={darkMode}
-        handleChangeMode={handleChangeMode}
-      />
-    </ConfigProvider>
+    <StateProvider>
+      <RouterProvider router={router} />
+    </StateProvider>
   );
 }
 
---
+++ root/frontend/src/state/atoms.ts
@@ -0,0 +1,31 @@
+import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
+
+const isSystemDarkMode = () => {
+  const prefersDarkMode =
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+  return prefersDarkMode;
+};
+
+export const darkModeAtom = atomWithStorage(
+  "app.darkMode",
+  false,
+  {
+    getItem: (key: string) => {
+      const storedValue = localStorage.getItem(key);
+      if (storedValue === null) {
+        return isSystemDarkMode();
+      }
+      return JSON.parse(storedValue);
+    },
+    setItem: (key: string, newValue: boolean) => {
+      localStorage.setItem(key, JSON.stringify(newValue));
+    },
+    removeItem: (key: string) => {
+      localStorage.removeItem(key);
+    },
+  },
+  { getOnInit: true },
+);
+export const sillyAtom = atom("silly");
