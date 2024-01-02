f7a410b44af9d6475f2703fbaeaf74d8ca0b42db
Gabrielle Jonsson <dev.gabriellejonsson@gmail.com>
2024-01-02 22:56:53 +0100

    Update types and files

--- root/frontend/src/components/datatable/TableColumns.tsx
+++ root/frontend/src/components/datatable/TableColumns.tsx
@@ -1,4 +1,4 @@
-import { Income } from "../../__mock__/mockDataTypes";
+import { Expense, Income } from "../../__mock__/mockDataTypes";
 import TableSorter from "../../util/tablesorter";
 
 const getFilters = (values: string[]) => {
@@ -27,6 +27,7 @@ export const incomeColumns = (dataSource: Income[]) => [
     dataIndex: "date",
     sorter: (a: Income, b: Income) => TableSorter.DATE(a.date, b.date),
     multiple: 1,
+    render: (date: Date) => new Date(date).toLocaleDateString("sv-SE"),
   },
   {
     title: "Förekomst",
@@ -56,6 +57,58 @@ export const incomeColumns = (dataSource: Income[]) => [
     multiple: 1,
     filters: getFilters(dataSource.map((income) => income.category)),
     onFilter: (value: string, record: Income) =>
+      record.category.indexOf(value) === 0,
+  },
+];
+
+export const expenseColumns = (dataSource: Expense[]) => [
+  {
+    title: "Titel",
+    dataIndex: "name",
+    sorter: (a: Expense, b: Expense) => TableSorter.STRING(a.name, b.name),
+    multiple: 1,
+  },
+  {
+    title: "Summa",
+    dataIndex: "amount",
+    sorter: (a: Expense, b: Expense) => TableSorter.NUMBER(a.amount, b.amount),
+    multiple: 1,
+  },
+  {
+    title: "Datum",
+    dataIndex: "date",
+    sorter: (a: Expense, b: Expense) => TableSorter.DATE(a.date, b.date),
+    multiple: 1,
+    render: (date: Date) => new Date(date).toLocaleDateString("sv-SE"),
+  },
+  {
+    title: "Förekomst",
+    dataIndex: "occurance",
+    sorter: (a: Expense, b: Expense) =>
+      TableSorter.STRING(a.occurance, b.occurance),
+    multiple: 1,
+    filters: getFilters(dataSource.map((expense) => expense.occurance)),
+    onFilter: (value: string, record: Expense) =>
+      record.occurance.indexOf(value) === 0,
+  },
+  {
+    title: "Mottagare",
+    dataIndex: "receiver",
+    sorter: (a: Expense, b: Expense) =>
+      a.receiver && b.receiver && TableSorter.STRING(a.receiver, b.receiver),
+    multiple: 1,
+    filters: getFilters(dataSource.map((expense) => expense.receiver ?? "")),
+    onFilter: (value: string, record: Expense) =>
+      record.receiver?.indexOf(value) === 0,
+  },
+  {
+    title: "Kategori",
+    dataIndex: "category",
+    sorter: (a: Expense, b: Expense) =>
+      TableSorter.STRING(a.category, b.category),
+    multiple: 1,
+    filters: getFilters(dataSource.map((expense) => expense.category)),
+    onFilter: (value: string, record: Expense) =>
       record.category.indexOf(value) === 0,
   },
 ];
