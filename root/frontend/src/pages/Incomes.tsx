749070d2376daa174555c1d04846d69d53bab85d
Gabrielle Jonsson <dev.gabriellejonsson@gmail.com>
2024-01-02 21:09:43 +0100

    Add form in modal to add income

--- root/frontend/src/pages/Incomes.tsx
+++ root/frontend/src/pages/Incomes.tsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import { Button, Flex } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 import withSuspense from "../util/hoc/withSuspense";
 import { mockIncome } from "../__mock__/mockData";
 import DataTable from "../components/datatable/DataTable";
 import { incomeColumns } from "../components/datatable/TableColumns";
+import IncomeFormModal from "../components/modals/IncomeFormModal";
 
 function Incomes() {
+  const [incomeModalOpen, setIncomeModalOpen] = useState(false);
+
   return (
     <Flex vertical gap="middle" justify="flex-start">
+      <IncomeFormModal
+        open={incomeModalOpen}
+        closeModal={() => setIncomeModalOpen(false)}
+      />
       <Flex vertical={false} justify="flex-end">
-        <Button type="primary" icon={<PlusOutlined />}>
+        <Button
+          type="primary"
+          icon={<PlusOutlined />}
+          onClick={() => setIncomeModalOpen((prev) => !prev)}
+        >
           Ny inkomst
         </Button>
       </Flex>
---
+++ root/frontend/src/util/types/IncomeTypes.ts
@@ -0,0 +1,14 @@
+export type Recurrance =
+  | "En gång"
+  | "Dagligen"
+  | "Veckovis"
+  | "Månadsvis"
+  | "Årsvis";
+
+export enum RecurranceEnum {
+  once = "En gång",
+  daily = "Dagligen",
+  weekly = "Veckovis",
+  monthly = "Månadsvis",
+  yearly = "Årsvis",
+}
