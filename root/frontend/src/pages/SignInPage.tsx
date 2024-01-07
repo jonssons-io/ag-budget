03b2ff4a1e27131e21abd05b9c70fe47f9c5ef0c
Gabrielle Jonsson <dev.gabriellejonsson@gmail.com>
2024-01-07 14:55:01 +0100

    Add basic layout for signup and signin

--- root/frontend/src/pages/SignInPage.tsx
+++ root/frontend/src/pages/SignInPage.tsx
@@ -1,5 +1,15 @@
 import { SignIn } from "@clerk/clerk-react";
+import { Flex } from "antd";
 
 export default function SignInPage() {
-  return <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />;
+  return (
+    <Flex
+      vertical={false}
+      justify="center"
+      align="center"
+      style={{ height: "100vh", width: "100vw" }}
+    >
+      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+    </Flex>
+  );
 }
--- root/frontend/src/pages/SignUpPage.tsx
+++ root/frontend/src/pages/SignUpPage.tsx
@@ -1,5 +1,15 @@
 import { SignUp } from "@clerk/clerk-react";
+import { Flex } from "antd";
 
 export default function SignUpPage() {
-  return <SignUp />;
+  return (
+    <Flex
+      vertical={false}
+      justify="center"
+      align="center"
+      style={{ height: "100vh", width: "100vw" }}
+    >
+      <SignUp />
+    </Flex>
+  );
 }
